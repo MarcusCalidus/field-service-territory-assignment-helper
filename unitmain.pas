dea8120a0d99217925c87f44229bb277c5ea8ee4
Marco Warm <Marco.Warm@birkenstock.com>
2022-09-04 09:47:43 +0200

    first UI tests

--- unitmain.pas
+++ unitmain.pas
@@ -5,7 +5,7 @@ unit unitMain;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, ExtCtrls;
 
 type
 
@@ -13,11 +13,16 @@ type
 
   TFormMain = class(TForm)
     ImageList1: TImageList;
+    ListView1: TListView;
+    PageControl1: TPageControl;
+    Splitter1: TSplitter;
     StatusBar1: TStatusBar;
+    StatusBar2: TStatusBar;
     ToolBar1: TToolBar;
     ToolButton1: TToolButton;
     procedure CoolBar1Change(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure PageControl1Change(Sender: TObject);
   private
 
   public
@@ -34,6 +39,11 @@ implementation
 { TFormMain }
 
 procedure TFormMain.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TFormMain.PageControl1Change(Sender: TObject);
 begin
 
 end;
