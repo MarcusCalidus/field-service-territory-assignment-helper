225d468133e729e3cea5723c5f101d211fa6f179
Marco Warm <Marco.Warm@birkenstock.com>
2022-09-10 13:45:44 +0200

    territory Master

--- src/unitdata.pas
+++ src/unitdata.pas
@@ -11,31 +11,44 @@ type
 
   generic TDataObject<T> = class
     Items: array of T;
+    Query: TSQLQuery;
+    Transaction: TSQLTransaction;
     constructor Create;
+    destructor Destroy;
     procedure ClearItems();
-    procedure Add(Value: T);
+    procedure Add(initialValues: TStrings; refreshData: boolean = false);
+    procedure Delete(ID: string; refreshData: boolean = false);
+    procedure Update(ID: string; newValues: TStrings; refreshData: boolean = false);
     procedure Refresh();
   end;
 
-  TCongregation = class(TObject)  
+  TCongregation = class(TObject)
         constructor Create(fields: TFields); overload;
   public
     tableName: string; static;
-        id: string;
-        name: string;
+    id: string;
+    name: string;
+  end;
+
+  TTerritory = class(TObject)
+    constructor Create(fields: TFields); overload;
+  public
+    tableName: string; static;
+    id: string;
+    name: string;
+    description: string;
   end;
 
   { TDataModuleMain }
 
   TDataModuleMain = class(TDataModule)
     DBConnection: TSQLite3Connection;
-    SQLQuery1: TSQLQuery;
-    SQLTransaction1: TSQLTransaction;
     procedure DataModuleCreate(Sender: TObject);
   private
 
   public
     congregations: specialize TDataObject<TCongregation>;
+    territories: specialize TDataObject<TTerritory>;
   end;
 
 var
@@ -54,11 +67,22 @@ begin
   self.name:=fields.FieldByName('name').asString;
 end;
 
+{ TTerritory }
+
+constructor TTerritory.Create(fields: TFields);
+begin
+  inherited Create;
+  self.id:=fields.FieldByName('id').AsString;
+  self.name:=fields.FieldByName('name').asString;
+  self.description:=fields.FieldByName('description').asString;
+end;
+
 { TDataModuleMain }
 
 procedure TDataModuleMain.DataModuleCreate(Sender: TObject);
 begin
   self.congregations:=specialize TDataObject<TCongregation>.Create;
+  self.territories:=specialize TDataObject<TTerritory>.Create;
 end;
 
 { TDataObject }
@@ -66,12 +90,96 @@ end;
 constructor TDataObject.Create;
 begin
   Items := [];
+  Query:=TSQLQuery.Create(DataModuleMain);
+  Transaction:=TSQLTransaction.Create(DataModuleMain);
+  Transaction.DataBase:=DataModuleMain.DBConnection; 
+  Query.Transaction:=Transaction;
 end;
 
-procedure TDataObject.Add(Value: T);
+destructor TDataObject.Destroy;
 begin
-  SetLength(Items, Length(Items) + 1);
-  Items[Length(Items) - 1] := Value;
+  Query.Free;
+  Transaction.Free;
+end;
+
+procedure TDataObject.Add(initialValues: TStrings; refreshData: boolean = false);
+var
+  i: integer;
+  names: String;
+  values: String;
+  uuid: TGuid;
+begin
+  CreateGUID(uuid);
+  initialValues.Values['ID']:=GUIDToString(uuid);
+  for i:=0 to initialValues.Count-1 do
+  begin
+    names := names + initialValues.Names[i];
+    values := values + ':'+ initialValues.Names[i];
+    if (i < initialValues.Count-1) then
+    begin
+       names := names +', ';
+       values := values +', ';
+    end;
+  end;
+  Query.SQL.Text:='insert into ' + T.tableName + ' (' + names + ') values (' + values + ');';
+  Transaction.StartTransaction;
+  Query.Prepare;
+  for i:=0 to initialValues.count - 1 do
+  begin
+    Query.ParamByName(initialValues.Names[i]).AsString:=initialValues.ValueFromIndex[i];
+  end;
+  try
+    Query.ExecSQL;
+    Transaction.Commit;
+  except
+    Transaction.Rollback
+  end;
+  if refreshData then
+    self.Refresh();
+end;
+
+procedure TDataObject.Delete(ID: String; refreshData: Boolean);
+begin
+  Query.SQL.Text:='delete from ' + T.tableName + ' where ID=''' + ID + '''';
+  Transaction.StartTransaction;
+  try
+    Query.ExecSQL;
+    Transaction.Commit;
+  except
+    Transaction.Rollback
+  end;
+  if refreshData then
+    self.Refresh();
+end;
+
+procedure TDataObject.Update(ID: String; newValues: TStrings; refreshData: boolean = false);
+var
+  i: integer;
+  setStr: String;
+begin
+  for i:=0 to newValues.Count-1 do
+  begin
+    setStr := setStr + newValues.Names[i] + '=' + ':'+ newValues.Names[i];
+    if (i < newValues.Count-1) then
+    begin
+       setStr := setStr +', ';
+    end;
+  end;
+  Query.SQL.Text:='update ' + T.tableName + ' set ' + setStr + ' where id=''' + ID + ''';';
+  Transaction.StartTransaction;
+  Query.Prepare;
+  for i:=0 to newValues.count - 1 do
+  begin
+    Query.ParamByName(newValues.Names[i]).AsString:=newValues.ValueFromIndex[i];
+  end;
+  try
+    Query.ExecSQL;
+    Transaction.Commit;
+  except
+    Transaction.Rollback
+  end;
+  if refreshData then
+    self.Refresh();
 end;
 
 procedure TDataObject.ClearItems();
@@ -86,18 +194,12 @@ begin
 end;
 
 procedure TDataObject.Refresh();
-var
-  query: TSQLQuery;
-  trans: TSQLTransaction;
 begin
   self.ClearItems();
-  query:=TSQLQuery.Create(DataModuleMain);
-  trans:=TSQLTransaction.Create(DataModuleMain);
-  trans.DataBase:=DataModuleMain.DBConnection;
-  query.Transaction:=trans;
+  Transaction.StartTransaction;
   try
     query.sql.text:='select * from ' + T.tableName;
-    trans.StartTransaction;
+
     query.open;
     while not query.EOF do
     begin
@@ -106,14 +208,14 @@ begin
       query.next;
     end;
     query.close;
-    trans.commit;
-  finally
-    query.Free;
-    trans.Free;
+    Transaction.commit;
+  except
+    Transaction.rollback;
   end;
 end;
 
 begin
-     TCongregation.tableName:='congregations';
+  TCongregation.tableName:='congregations';
+  TTerritory.tableName:='territories';
 end.
 
--- src/unitsettings.pas
+++ src/unitsettings.pas
@@ -5,7 +5,7 @@ unit unitSettings;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, StdCtrls,
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls,
   ExtCtrls, Buttons, ValEdit, unitData;
 
 type
@@ -13,14 +13,45 @@ type
   { TFormSettings }
 
   TFormSettings = class(TForm)
+    btnAddTerritory: TBitBtn;
+    btnDeleteTerritory: TBitBtn;
+    btnRefreshTerritories: TBitBtn;
+    btnSaveCongregation: TBitBtn;
     btnRefreshCongregations: TBitBtn;
+    btnAddCongregation: TBitBtn;
+    btnDeleteCongregation: TBitBtn;
+    btnSaveTerritory: TBitBtn;
+    ImageList1: TImageList;
     ListViewCongregations: TListView;
+    ListViewTerritories: TListView;
     PageControl1: TPageControl;
+    Panel3: TPanel;
+    PanelTerritoryTitle: TPanel;
+    PanelTerritory: TPanel;
     PanelCongregation: TPanel;
+    Panel2: TPanel;
+    PanelCongregationTitle: TPanel;
+    PanelCongregationToolbar: TPanel;
+    PanelTerritoryToolbar: TPanel;
+    Splitter1: TSplitter;
+    Splitter2: TSplitter;
+    TabSheetTerritory: TTabSheet;
     TabSheetCongregation: TTabSheet;
     ValueListEditorCongregation: TValueListEditor;
+    ValueListEditorTerritory: TValueListEditor;
+    procedure btnAddCongregationClick(Sender: TObject);
+    procedure btnAddTerritoryClick(Sender: TObject);
+    procedure btnDeleteCongregationClick(Sender: TObject);
+    procedure btnDeleteTerritoryClick(Sender: TObject);
     procedure btnRefreshCongregationsClick(Sender: TObject);
+    procedure btnRefreshTerritoriesClick(Sender: TObject);
+    procedure btnSaveCongregationClick(Sender: TObject);
+    procedure btnSaveTerritoryClick(Sender: TObject);
+    procedure ListViewCongregationsEditing(Sender: TObject; Item: TListItem;
+      var AllowEdit: Boolean);
     procedure ListViewCongregationsSelectItem(Sender: TObject; Item: TListItem;
+      Selected: Boolean);
+    procedure ListViewTerritoriesSelectItem(Sender: TObject; Item: TListItem;
       Selected: Boolean);
   private
 
@@ -47,8 +78,84 @@ begin
     li.Caption:=DataModuleMain.congregations.Items[i].id;
     li.SubItems.add(DataModuleMain.congregations.Items[i].name);
   end;
+end;
+
+procedure TFormSettings.btnRefreshTerritoriesClick(Sender: TObject);
+var
+  i: Integer;
+  li: TListItem;
+begin
+  dataModuleMain.territories.refresh;
+  ListViewTerritories.Items.Clear;
+  for i:=0 to Length(DataModuleMain.territories.Items) - 1 do
+  begin
+    li:=ListViewTerritories.Items.Add;
+    li.Caption:=DataModuleMain.territories.Items[i].id;
+    li.SubItems.add(DataModuleMain.territories.Items[i].name);
+    li.SubItems.add(DataModuleMain.territories.Items[i].description);
+  end;
+end;
+
+procedure TFormSettings.btnSaveCongregationClick(Sender: TObject);
+begin
+  DataModuleMain.congregations.update(
+     ListViewCongregations.Selected.caption,
+     ValueListEditorCongregation.Strings);
+  btnRefreshCongregations.Click;
+end;
+
+procedure TFormSettings.btnSaveTerritoryClick(Sender: TObject);
+begin
+  DataModuleMain.territories.update(
+     ListViewTerritories.Selected.caption,
+     ValueListEditorTerritory.Strings);
+  btnRefreshTerritories.Click;
+end;
+
+procedure TFormSettings.ListViewCongregationsEditing(Sender: TObject;
+  Item: TListItem; var AllowEdit: Boolean);
+begin
+  AllowEdit:=False;
+end;
+
+procedure TFormSettings.btnDeleteCongregationClick(Sender: TObject);
+begin
+  DataModuleMain.congregations.delete(ListViewCongregations.Selected.caption);
+  btnRefreshCongregations.Click;
+end;
+
+procedure TFormSettings.btnDeleteTerritoryClick(Sender: TObject);
+begin
+  DataModuleMain.territories.delete(ListViewTerritories.Selected.caption);
+  btnRefreshTerritories.Click;
+end;
+
+procedure TFormSettings.btnAddCongregationClick(Sender: TObject);
+var
+  initial: TStringList;
+begin
+  initial:=TStringList.Create;
+  initial.Values['name']:='New Congregation';
+  DataModuleMain.congregations.Add(initial);
+  initial.free;
+  btnRefreshCongregations.click;
 
 end;
+
+procedure TFormSettings.btnAddTerritoryClick(Sender: TObject);
+var
+  initial: TStringList;
+begin
+  initial:=TStringList.Create;
+  initial.Values['name']:='Territory';
+  try
+     DataModuleMain.territories.Add(initial);
+  finally
+      initial.free;
+      btnRefreshTerritories.click;
+  end;
+end;
+
 
 procedure TFormSettings.ListViewCongregationsSelectItem(Sender: TObject;
   Item: TListItem; Selected: Boolean);
@@ -56,14 +163,55 @@ var
   i: integer;
 begin
   ValueListEditorCongregation.Clear;
-  for i:=0 to Item.SubItems.Count - 1 do
+  if Selected then
   begin
-    ValueListEditorCongregation.InsertRow(
-        ListViewCongregations.Columns[i+1].Caption,
-        Item.SubItems[i],
-        true);
+    PanelCongregationTitle.Caption:='Congregation ID ' + Item.Caption;
+    PanelCongregationToolbar.Tag:=Item.Index;
+    for i:=0 to Item.SubItems.Count - 1 do
+    begin
+      ValueListEditorCongregation.InsertRow(
+          ListViewCongregations.Columns[i+1].Caption,
+          Item.SubItems[i],
+          true);
+    end;
+  end
+  else
+  begin
+    PanelCongregationToolbar.Tag := -1;
+    PanelCongregationTitle.Caption:='No Congregation Selected'
   end;
+
+  btnDeleteCongregation.enabled:=Selected;
+  PanelCongregation.Enabled:=Selected;
 end;
+
+procedure TFormSettings.ListViewTerritoriesSelectItem(Sender: TObject;
+  Item: TListItem; Selected: Boolean);
+var
+  i: integer;
+begin
+  ValueListEditorTerritory.Clear;
+  if Selected then
+  begin
+    PanelTerritoryTitle.Caption:='Territory ID ' + Item.Caption;
+
+    for i:=0 to Item.SubItems.Count - 1 do
+    begin
+      ValueListEditorTerritory.InsertRow(
+          ListViewTerritories.Columns[i+1].Caption,
+          Item.SubItems[i],
+          true);
+    end;
+  end
+  else
+  begin
+    PanelTerritoryTitle.Caption:='No Territory Selected'
+  end;
+
+  btnDeleteTerritory.enabled:=Selected;
+  PanelTerritory.Enabled:=Selected;
+end;
+
 
 end.
 
